ae0b79c3af57b11504cb69dfc4144c8bbd7e19f7
ally-petitt <allypetitt@gmail.com>
2021-08-25 20:17:39 -0700

    made join page and added socket connection event in server

--- server/index.js
+++ server/index.js
@@ -10,4 +10,14 @@ const server = http.createServer(app)
 const io = socketio(server)
 app.use(router)
 
+// socket is the client that joined
+io.on("connection", (socket) => {
+    console.log("we have a new connection")
+
+    socket.on("disconnect", () => {
+        console.log("user left the chat")
+
+    })
+})
+
 server.listen(PORT, () => console.log("Server has started on port " + PORT))
