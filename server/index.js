3d07043d911550f8c041225ee5c965c98f82c722
ally-petitt <allypetitt@gmail.com>
2021-08-26 21:16:48 -0700

    fixed cors issue and added full lifecycle of client socket

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const PORT = process.env.PORT || 4000
 
 const app = express();
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, { cors: {origin: "http://localhost:3000", methods: ["GET", "POST"]}})
 app.use(router)
 
 // socket is the client that joined
 io.on("connection", (socket) => {
     console.log("we have a new connection")
+
+    socket.on("join", ({ name, room }) => {
+        console.log("the name is " + name)
+    })
 
     socket.on("disconnect", () => {
         console.log("user left the chat")
